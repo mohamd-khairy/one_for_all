89b403e3a90f6972601cabc20546826a4cd7c0ea
mohamd-khairy <mohamed.khairy.eg@gmail.com>
2021-08-07 13:05:52 +0200

    fix

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,6 +31,6 @@ class AuthServiceProvider extends ServiceProvider
         Passport::refreshTokensExpireIn(now()->addDays(30));
         Passport::personalAccessTokensExpireIn(now()->addMonths(6));
         
-        // Passport::loadKeysFrom(__DIR__ . '/../secrets/oauth');
+        Passport::loadKeysFrom(__DIR__ . '/../secrets/oauth');
     }
 }
